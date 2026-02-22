4ea295e170e99a6552c8e86274624a8eb688b0c2
순수연 <48740214+PureKite@users.noreply.github.com>
2026-02-22 20:46:33 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.09 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -0,0 +1,24 @@
+def solution(n, w, num):
+    warehouse = []
+    
+    for i in range(1, n+1, w):
+        row = list(range(i, min(i+w, n+1)))
+        
+        if len(row) <= w:
+            row += [0] * (w - len(row))
+            
+        if (i // w) % 2 == 1:
+            row.reverse()
+            
+        warehouse.append(row)
+        
+    for k in warehouse:
+        if num in k:
+            floor, rack = warehouse.index(k), k.index(num)
+            break
+            
+    if warehouse[-1][rack] != 0:
+        answer = len(warehouse) - floor
+    else:
+        answer = len(warehouse) - floor - 1
+    return answer
